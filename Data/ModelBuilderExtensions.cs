7e62ceb83bbaacbc52c1e523ae1421276d02e5ca
Colin Collins <epiccolin1950@gmail.com>
2024-11-30 14:24:28 -0500

    Initial commit of TeamProjectWebAPI

---
+++ Data/ModelBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamProjectWebAPI.Models;
+
+namespace TeamProjectWebAPI.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hobby>().HasData(
+                new Hobby { Id = 1, Name = "Fishing", Description = "Outdoor Activity", Frequency = "Twice a Week", DurationInMinutes = 180});
+
+            modelBuilder.Entity<TeamMember>().HasData(
+                new TeamMember { Id = 1, FullName = "Kyle Rieve", Birthdate = 6251992, CollegeProgram = "Information Technology", YearInProgram = "Sophomore" });
+
+            modelBuilder.Entity<Sport>().HasData(
+                new Sport { Id = 1, SportName = "Football", TeamOrIndividual = "Ohio State", PlayersCount = 11, Season = "Fall" });
+
+            modelBuilder.Entity<Food>().HasData(
+               new Food { Id = 1, FoodName = "Pizza", CuisineType = "Italian", IsSpicy = false, Calories = 285 });
+        }
+    }
+}
---
+++ Models/Hobby.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamProjectWebAPI.Models
+{
+    public class Hobby
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Frequency { get; set; } = string.Empty;
+        public int DurationInMinutes { get; set; }
+    }
+}
