a2821c91814035db0494d3b6468ddd66ecda321e
ZacharyDunn1120 <dunnzd@mail.uc.edu>
2024-12-05 19:04:09 -0500

    Initial commit: Add TeamProjectWebAPI source code

---
+++ TeamProjectWebAPI-main/Models/Food.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamProjectWebAPI.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string FoodName { get; set; } = string.Empty;
+        public string CuisineType { get; set; } = string.Empty;
+        public bool IsSpicy { get; set; }
+        public int Calories { get; set; }
+    }
+}
